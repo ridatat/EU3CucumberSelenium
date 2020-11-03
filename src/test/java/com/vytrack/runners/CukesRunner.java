c0612437eb03b6a4996bb2ae5b751a511132330d
Rida <ridatat@gmail.com>
2020-11-03 16:12:27 +0300

    change tags

--- src/test/java/com/vytrack/runners/CukesRunner.java
+++ src/test/java/com/vytrack/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
         dryRun = false,
-        tags = "@understand"
+        tags = "@smoke"
 
 )
 public class CukesRunner {
